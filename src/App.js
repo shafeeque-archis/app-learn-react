fa23ecfa142c7ea7720a56128002a52ae26b64b9
shafeeque-archis <shafeeque.archis@gmail.com>
2021-05-09 14:35:19 +0530

    Lifecycle example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import ReactDOM from 'react-dom'
 import './App.css';
 
 import ClassComponent from './01_ClassComponent/01_ClassComponent';
 import FunctionalComponent from './02_FunctionalComponent/02_FunctionalComponent';
+import Clock from './Clock/Clock'
+
+
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Components</h1>
       <h2>01 Class component</h2>
       <ClassComponent name="PROP"/>
       <FunctionalComponent name="Functional component PROP"/>
+      <Clock />
     </div>
   );
 }
