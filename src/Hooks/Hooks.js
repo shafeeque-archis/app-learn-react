0e2eb8bc49e7dd3adba0897f4c6941e3dca3bbd7
shafeeque-archis <shafeeque.archis@gmail.com>
2021-05-14 16:36:28 +0530

    Hooks

--- src/Hooks/Hooks.js
+++ src/Hooks/Hooks.js
@@ -1,35 +1,68 @@
 import React, { useEffect, useState } from 'react'
 
 const Hooks = () => {
-    const [name, setName] = useState("Shafeeque");
-    const [surname, setSurname] = useState("Naripatta");
+    // These are custom hook
+    const name = useFormInput("Shafeque");
+    const surname = useFormInput('Naripatta');
+    useDocumentTitle(name.value +' '+ surname.value);
+    const width = useWidth()
     
-    const changeNameHandler = (e) => {
-        setName(e.target.value)
-    }
 
-    const changeSurnameHandler = (e) => {
-        setSurname(e.target.value)
-    }
+    // const changeNameHandler = (e) => {
+    //     setName(e.target.value)
+    // }
 
-    useEffect(() => {
-        document.title = name +' '+ surname;
-    })
+    // const changeSurnameHandler = (e) => {
+    //     setSurname(e.target.value)
+    // }
+
+    // useEffect(() => {
+    //     document.title = name +' '+ surname;
+    // })
 
     return (
         <div>
-            <label>{name} {surname}</label><br/>
-            <input
-                value={name}
-                onChange={changeNameHandler}
-            />
+            <input { ...name }/>
             <br />
-            <input
-                value={surname}
-                onChange={changeSurnameHandler}
-            />
+            <input { ...surname } />
+            <br />
+            {width}
         </div>
     )
+}
+
+function useDocumentTitle(title) {
+    useEffect(() => {
+        document.title = title;
+    })
+}
+
+const useFormInput = (initValue) => {
+    const [value, setValue] = useState(initValue);
+
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    return {
+        value,
+        onChange: handleChange
+    };
+}
+
+// implement window width 
+
+const useWidth = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    })
+
+    return width
 }
 
 export default Hooks;
